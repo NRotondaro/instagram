3c6a1c4ba264847351df70a6a6f7422a16d89e95
NRotondaro <niksonrotondaro1@gmail.com>
2022-02-11 14:20:01 -0300

    feat(post): added a post with static data

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -1,3 +1,4 @@
+import { Posts } from './Posts'
 import { Stories } from './Stories'
 
 export const Feed = () => {
@@ -5,6 +6,7 @@ export const Feed = () => {
     <main className="mx-auto grid grid-cols-1 md:max-w-5xl md:grid-cols-2 xl:max-w-7xl xl:grid-cols-3">
       <section className="col-span-2">
         <Stories />
+        <Posts />
       </section>
 
       <section></section>
---
+++ components/Post.tsx
@@ -0,0 +1,56 @@
+import { PostProps } from './Posts'
+import {
+  BookmarkIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  EmojiHappyIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+} from '@heroicons/react/outline'
+import { HeartIcon as HeartIconFilled } from '@heroicons/react/solid'
+
+export const Post = ({ id, username, userImg, img, caption }: PostProps) => {
+  return (
+    <div className="my-7 rounded-sm border bg-white">
+      <div className="flex items-center p-5">
+        <img
+          src={userImg}
+          className="mr-3 h-12 w-12 rounded-full border object-contain p-1"
+          alt="user-pic"
+        />
+        <p className="flex-1 font-bold">{username}</p>
+        <DotsHorizontalIcon className="h-5" />
+      </div>
+      <div>
+        <img className="w-full object-cover" src={img} alt="post-pic" />
+      </div>
+      <div>
+        <div className="flex justify-between px-4 pt-4">
+          <div className="flex space-x-4">
+            <HeartIcon className="btn" />
+            <ChatIcon className="btn" />
+            <PaperAirplaneIcon className="btn rotate-90" />
+          </div>
+          <div>
+            <BookmarkIcon className="btn" />
+          </div>
+        </div>
+
+        <p className="truncate p-5">
+          <span className="mr-1 font-bold">{username}</span>
+          {caption}
+        </p>
+      </div>
+      <div></div>
+      <form className="flex items-center p-4">
+        <EmojiHappyIcon className="h-7" />
+        <input
+          className="flex-1 border-none focus:ring-0"
+          placeholder="Adicione um comentário..."
+          type="text"
+        />
+        <button className="font-semibold text-blue-400">Post</button>
+      </form>
+    </div>
+  )
+}
