8df982b20e04e9a65e2ac3478c75d2942e7a8e90
NRotondaro <niksonrotondaro1@gmail.com>
2022-02-11 08:56:29 -0300

    feat(stories): added suggestions variable and respective types

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,14 +1,43 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import faker from '@faker-js/faker'
 
+interface SuggestionsProps {
+  [index: number]: {
+    id: number
+    name: string
+    username: string
+    avatar: string
+    email: string
+    dob: Date
+    phone: string
+    adress: {
+      street: string
+      suite: string
+      city: string
+      zipcode: string
+      geo: {
+        lat: string
+        lng: string
+      }
+    }
+    website: string
+    company: {
+      name: string
+      catchPhrase: string
+      bs: string
+    }
+  }
+}
+
 export const Stories = () => {
+  const [suggestions, setSuggestions] = useState<SuggestionsProps[]>([])
+
   useEffect(() => {
     const suggestions = [...Array(20)].map((_, i) => ({
       ...faker.helpers.contextualCard(),
       id: i,
     }))
-
-    console.log(suggestions);
+    setSuggestions(suggestions)
   }, [])
 
   return <div>Olá</div>
